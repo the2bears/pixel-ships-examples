e163aa3f7b8d241135d57db5ac8f8ac331c46ee0
William Swaney <the2bears@gmail.com>
2016-06-18 21:52:14 -0700

    Initial Checkin

---
+++ pixel-ship-lanterna/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pixel-ship-lanterna
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ pixel-ship-lanterna/src/pixel_ship_lanterna/core.clj
@@ -0,0 +1,16 @@
+(ns pixel-ship-lanterna.core
+  (:require [lanterna.screen :as console])
+  (:gen-class))
+
+(def scr (console/get-screen))
+
+(defn -main
+  [& args]
+    (do (console/start scr)
+      (console/put-string scr 10 10 "Hello, pixel-ships!")
+      (console/put-string scr 10 11 "Press any key to exit!")
+      (console/redraw scr)
+      (console/get-key-blocking scr)
+    (console/stop scr)))
+
+
