c32ff81516a013946dfc343b3936729f357f6251
William Swaney <the2bears@gmail.com>
2016-06-25 22:13:11 -0700

    Implemented rendering of pixel-ship

--- pixel-ship-quil/project.clj
+++ pixel-ship-quil/project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [quil "2.4.0"]])
+                 [quil "2.4.0"]
+                 [pixel-ships "0.1.0-SNAPSHOT"]]
+  :main pixel-ship-quil.core)
--- pixel-ship-quil/src/pixel_ship_quil/core.clj
+++ pixel-ship-quil/src/pixel_ship_quil/core.clj
@@ -1,47 +1,58 @@
 (ns pixel-ship-quil.core
   (:require [quil.core :as q]
-            [quil.middleware :as m]))
+            [quil.middleware :as m]
+            [pixel-ships.core :as psc :refer :all]
+            [pixel-ships.bollinger :as bollinger :refer :all]))
+
+(declare create-pixel-ship-quil draw-rect)
 
 (defn setup []
   ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
   ; Set color mode to HSB (HSV) instead of default RGB.
-  (q/color-mode :hsb)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color 0
-   :angle 0})
+  (q/color-mode :hsb 1.0)
+  ; create ship and put it into state
+  {:pixel-ship (create-pixel-ship-quil)})
 
 (defn update-state [state]
-  ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+  state)
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
-  (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+  (q/background 0.9)
+  (let [hull-pixels ((comp :hull :pixels) (:pixel-ship state))
+        solid-pixels ((comp :solid :pixels) (:pixel-ship state))
+        cockpit-pixels ((comp :cockpit :pixels) (:pixel-ship state))]
+    (q/with-translation [200
+                         (- (q/height) 200)]
 
-(q/defsketch pixel-ship-quil
-  :title "You spin my circle right round"
-  :size [500 500]
-  ; setup function called only once, during sketch initialization.
-  :setup setup
-  ; update-state is called on each iteration before draw-state.
-  :update update-state
-  :draw draw-state
-  :features [:keep-on-top]
-  ; This sketch uses functional-mode middleware.
-  ; Check quil wiki for more info about middlewares and particularly
-  ; fun-mode.
-  :middleware [m/fun-mode])
+      (doseq [pixel hull-pixels]
+        (draw-rect pixel))
+      (doseq [pixel solid-pixels]
+        (draw-rect pixel))
+      (doseq [pixel cockpit-pixels]
+        (draw-rect pixel))
+    )))
+
+(defn -main
+  []
+  (q/sketch
+    :title "An example of a pixel-ship in Quil"
+    :size [800 600]
+    :setup setup
+    ;:update update-state
+    :draw draw-state
+    :features [:keep-on-top]
+    :middleware [m/fun-mode] ))
+
+;(-main)
+
+(defn create-pixel-ship-quil []
+  (psc/color-pixel-ship (psc/create-pixel-ship (assoc bollinger/model :seed Integer/MAX_VALUE))));(rand-int Integer/MAX_VALUE)))))
+
+(def p-per-c 20)
+
+(defn draw-rect [{:keys [x y color]}]
+  (q/fill (:h color) (:s color) (:v color))
+  (q/rect (- (* x p-per-c) (* 6 p-per-c)) (- (* y p-per-c) (* 6 p-per-c)) p-per-c p-per-c))
+
